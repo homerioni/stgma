ae2fd28426b8a799f46fb96bcc5b044598a75fc2
homerioni <homerion13@gmail.com>
2022-06-30 21:19:41 +0300

    uo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 'use strict'
 
 $(document).ready(function () {
+
+    // tabs for table
+    $('.tabs-table__tab').click(function () {
+        $(this).parent().find('.tabs-table__tab').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.tabs-table__tab-content input').change(function () {
+        $(this).parents('.tabs-table').find('.tabs-table__tab-content').removeClass('active');
+        $(this).parent().addClass('active');
+    });
+
 });
